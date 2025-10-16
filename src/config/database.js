0508f37368ab4decc2942bcf94b35340333f1b15
Bagus Maulana <bagusmaulana0320@gmail.com>
2025-10-16 21:12:19 +0700

    fix: memperbaiki kesalahan penulisan untuk MYSQL_DATABASE

--- src/config/database.js
+++ src/config/database.js
@@ -1,22 +1,23 @@
-const mysql = require('mysql2/promise');
-require('dotenv').config(); // Biarkan baris ini untuk development lokal
+const mysql = require("mysql2/promise");
+require("dotenv").config(); // Biarkan baris ini untuk development lokal
 
-// Tambahkan log di sini
-console.log('Database host:', process.env.MYSQLHOST);
-console.log('Database user:', process.env.MYSQLUSER);
-console.log('Database password:', process.env.MYSQLPASSWORD);
-console.log('Database database:', process.env.MYSQLDATABASE);
-console.log('Database port:', process.env.MYSQLPORT);
+console.log("Connecting to MySQL with:");
+console.log({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQLPORT,
+});
 
 const pool = mysql.createPool({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
+    database: process.env.MYSQL_DATABASE,
     port: process.env.MYSQLPORT,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
 });
 
 module.exports = pool;
