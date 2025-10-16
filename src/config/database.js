c0e5cc880a32f466694741e0ffef2782100f66b1
Bagus Maulana <bagusmaulana0320@gmail.com>
2025-10-16 20:59:31 +0700

    add log

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,13 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config(); // Biarkan baris ini untuk development lokal
 
+// Tambahkan log di sini
+console.log('Database host:', process.env.MYSQLHOST);
+console.log('Database user:', process.env.MYSQLUSER);
+console.log('Database password:', process.env.MYSQLPASSWORD);
+console.log('Database database:', process.env.MYSQLDATABASE);
+console.log('Database port:', process.env.MYSQLPORT);
+
 const pool = mysql.createPool({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
